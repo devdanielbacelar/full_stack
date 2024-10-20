3c28ae1f5c900cc4935a30afd66011adfb82c2b0
Daniel Bacelar <developer.danielbacelar@gmail.com>
2024-10-20 09:24:46 -0300

    Alterações e adição de documentos dia 20/10 - 09hrs

--- aulas/aula12/js/index.js
+++ aulas/aula12/js/index.js
@@ -1,7 +1,14 @@
 let varA = 'A'; //B
-let varB = 'B'; //A
-let varC = 'C'; //C
+let varB = 'B'; //C
+let varC = 'C'; //A
 
+/*
+const varATemp = varA;
+varA = varB;
+varB = varC;
+varC = varATemp;
+*/
 
+[varA, varB, varC] = [varB, varC, varA]
 
 console.log(varA, varB, varC);
