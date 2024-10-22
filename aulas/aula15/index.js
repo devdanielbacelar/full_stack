7dea077aa43132db2127aa13af0c1167da2e64d2
Daniel Bacelar <developer.danielbacelar@gmail.com>
2024-10-22 18:54:32 -0300

    Alterações e adição de documentos dia 22/10 - 18hrs

---
+++ aulas/aula15/index.js
@@ -0,0 +1,9 @@
+//IEEE 754-2008
+let num1 = 1;
+let num2 = 2.5;
+
+//console.log(num1.toString() + num2);
+//num1 = num1.toString();
+//console.log(num1.toString(2));
+//console.log(num1.toFixed.toFixed(2));
+//console.log(Number.isInteger(num1));
---
+++ aulas/aula15/teste/index.js
@@ -0,0 +1,9 @@
+let num1 = 9.49;
+
+//let num2 = Math.floor(num1);
+//let num2 = Math.ceil(num1);
+//let num2 = Math.round(num1);
+//console.log(Math.max(1,2,3,4,5, -10, -50, 1550, 9,8,7,6,5));
+//console.log(Math.min(1,2,3,4,5, -10, -50, 1550, 9,8,7,6,5));
+const aleatorio = Math.round(Math.random() * (10 - 5) +5);
+console.log(aleatorio);
